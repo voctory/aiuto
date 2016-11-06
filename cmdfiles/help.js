99e3ef3115fea9fc457981899a881aee9b5d866f
voctory <Voctor@Van.local>
2016-11-05 20:54:19 -0700

    Added a description thing for commands

--- cmdfiles/help.js
+++ cmdfiles/help.js
@@ -3,9 +3,8 @@ const fs = require("fs");
 var commandsList = [];
 var activeCommands = JSON.parse(fs.readFileSync('./cmds.json', 'utf8'));
 for (var i = 0; i < activeCommands.commands.length; i++) {
-	commandsList.push(activeCommands.commands[i].name)
+	commandsList.push(`${activeCommands.commands[i].name} (${activeCommands.commands[i].description})`);
 	if (i + 1 == activeCommands.commands.length) {
 		console.log(`Aiuto's Commands:\n\n- ${commandsList.toString().replace(/,/gi, "\n- ")}`);
 	}
 }
-//console.log("Aiuto's Commands:\n\n- help\n- exit")
